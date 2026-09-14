cba8c814e506d883e2ec7ee4f78663cf288e0d97
litzrsh <litzrsh@gmail.com>
2026-09-15 02:39:33 +0900

    All reset

--- core/src/main/java/kkdugi/core/crypto/PropertyDecryptionException.java
+++
@@ -1,8 +0,0 @@
-package kkdugi.core.crypto;
-
-public class PropertyDecryptionException extends RuntimeException {
-
-    public PropertyDecryptionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
